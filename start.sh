e372777143799a553f20d482767722e0094ca3c8
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-04-27 10:46:28 +0000

    v0.1 adding the text editors

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ read -p "Hey! Please input  your username: " username
 
 # updating  and upgrading
 lsb_release -a
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # packages to install
@@ -17,12 +19,15 @@ packages=(
     alsa
     pulseaudio
     brightnessctl
+    dunst
     acpi
     i3
     lxrandr
     suckless-tools
     pcmanfm
     firefox
+    geany
+    sublime-text
     python3
     python3-pip
 )
