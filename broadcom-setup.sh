8ee512b71cc6a4d5b6c9236cf271579a633776ab
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-04-27 07:08:39 +0000

    v0.0 adding broadcom-wl

---
+++ broadcom-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+lspci -nn | grep Network
+
+sudo apt-get update 
+sudo apt-get dist-upgrade
+
+sudo apt-get install wireless-tools network-manager wpasupplicant 
+
+sudo apt-get install linux-image-$(uname -r | sed 's,[^-]*-[^-]*-,,')
+sudo apt-get install linux-headers-$(uname -r | sed 's,[^-]*-[^-]*-,,')
+
+sudo apt-get install broadcom-sta-dkms
+
+modules=(
+    b44
+    b43
+    ssb
+    brcmsmac
+    bcma
+)
+for module in ${modules[@]};do
+    sudo modprobe -r $module
+done
+sudo modprobe wl
+
