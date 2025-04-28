46126db84f8ab1f9f57e36158d3eb11bc7cc782b
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-04-28 10:43:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -f ~/.Xresources dots/Xresources
-cp -f ~/.config/i3/config dots/config
+cp -f ~/.Xresources ./dots/xorg/Xresources
+cp -f ~/.config/i3/config ./dots/i3/config
 
 echo "Updated successfuly"
