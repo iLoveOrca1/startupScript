4c5c44f5b29e5280b527f08d0b64092cccb79b3f
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-05-12 15:01:34 +0800

    modifiy config.sh, add new file, removing wallpaper

--- config.sh
+++ config.sh
@@ -10,13 +10,15 @@ cat /etc/X11/xinit/xinitrc > /home/$username/.xinitrc
 chmod +x /home/$username/.xinitrc
 echo "i3" >> /home/$username/.xinitrc
 
-cp -r ./wallpapers /home/$username/.config/
-
 cp ./dots/xorg/Xresources /home/$username/.Xresources
 xrdb -load /home/$username/.Xresources
 
+cat ./dots/bashrc/bashrc >> /home/$username/.bashrc
+
 mkdir -p /home/$username/.local/share/rofi/themes
-cp ./dots/rofi/squared-nord.rasi /home/$username/.local/share/rofi/themes/.
+cp ./dots/rofi/* /home/$username/.local/share/rofi/themes/.
 echo ""
+
 echo "Hey! Please run rofi-theme-selector on the terminal to change the rofi theme"
 sleep 3
+
--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ read -p "Hey! Please input  your username: " username
 
 # updating  and upgrading
 lsb_release -a
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # packages to install
@@ -14,7 +12,6 @@ packages=(
     network-manager
     rxvt-unicode
     vim
-    emacs
     alsa
     pulseaudio
     brightnessctl
@@ -23,10 +20,13 @@ packages=(
     i3
     lxrandr
     suckless-tools
+    rofi
     pcmanfm
+    vlc
+    flameshot
+    redshift
     firefox
     geany
-    sublime-text
     python3
     python3-pip
 )
