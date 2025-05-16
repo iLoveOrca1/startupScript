c50fdff76f2fe6a5cf2c2f4f31fd911b5f098a02
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-05-17 07:18:00 +0800

    Changes to be committed: 	modified:   README.md 	modified:   config.sh 	modified:   dots/i3/config 	new file:   dots/i3status/config 	modified:   dots/xorg/Xresources

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My dotfiles and my scripts everytime I fresh install linux
 
-Keep in mind yhat I DO NOT own any of the code I just ripped it off from some actually talented dudes from the Internet.
+Keep in mind that I DO NOT own any of the code I just ripped it off from some actually talented dudes from the Internet.
--- config.sh
+++ config.sh
@@ -3,20 +3,22 @@
 read -p "Hey! Please input  your username: " username
 
 mkdir -p /home/$username/.config/i3 
+mkdir -p /home/$username/.config/i3status
+mkdir -p /home/$username/.local/share/rofi/themes
+
 cp ./dots/i3/config /home/$username/.config/i3/config
-chmod +x /home/$username/.config/i3/config
-
-cat /etc/X11/xinit/xinitrc > /home/$username/.xinitrc
-chmod +x /home/$username/.xinitrc
-echo "i3" >> /home/$username/.xinitrc
-
+cp ./dots/i3status/config /home/$username/.config/i3status/config
 cp ./dots/xorg/Xresources /home/$username/.Xresources
-xrdb -load /home/$username/.Xresources
+cp ./dots/rofi/* /home/$username/.local/share/rofi/themes/.
 
 cat ./dots/bashrc/bashrc >> /home/$username/.bashrc
+cat /etc/X11/xinit/xinitrc > /home/$username/.xinitrc
 
-mkdir -p /home/$username/.local/share/rofi/themes
-cp ./dots/rofi/* /home/$username/.local/share/rofi/themes/.
+chmod +x /home/$username/.xinitrc
+chmod +x /home/$username/.config/i3/config
+echo "i3" >> /home/$username/.xinitrc
+
+xrdb -load /home/$username/.Xresources
 echo ""
 
 echo "Hey! Please run rofi-theme-selector on the terminal to change the rofi theme"
